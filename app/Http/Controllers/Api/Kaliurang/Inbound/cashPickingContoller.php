96749247b821a2924ded748977af9d813e7ec6bf
kimlun48 <asepsuherman1984@gmail.com>
2024-09-07 10:00:41 +0700

    70924

---
+++ app/Http/Controllers/Api/Kaliurang/Inbound/cashPickingContoller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Kaliurang\Inbound;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\kaliurang\inbound\CashPicking;
+
+class cashPickingContoller extends Controller
+{
+    protected $warehouse;
+    
+    public function __construct()
+    {
+        $this -> warehouse = '01003001';
+    }
+
+    public function getCashPicking ()
+    {
+        try {
+            $cashpicking = CashPicking::getCashPicking($this->warehouse);
+            if (empty($cashpicking)) {
+                return response()->json(['message' => 'No orders cash picking'], 404);
+            }
+            return response()->json($cashpicking);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+        }
+    }
+}
